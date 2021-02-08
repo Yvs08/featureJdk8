8922a808fb8d85bae759512bf10fbe4018b421dd
Yvs08 <yveslambenou@gmail.com>
2021-02-08 22:21:36 +0100

    commit first

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,8 @@
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Test {
     // instance method of the outer class
@@ -14,10 +19,39 @@ public class Test {
         // Accessing the inner class
         MethodInner_Demo inner = new MethodInner_Demo();
         inner.print();
+
+
+    }
+
+     Integer doSomething(Integer value) {
+        int test = 1;
+        int teste = 1 ;
+        try {
+             teste = value / test;
+
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
+
+        return teste;
     }
 
     public static void main(String args[]) {
         Test outer = new Test();
-        outer.my_Method();
+
+        Stream<Integer> myList = Stream.of(1, 2, 3, 1, 2, 3);
+
+        List<Integer> result = myList.
+                map(value -> { return outer.doSomething(value);}).
+                collect(Collectors.toList());
+
+        result.stream().forEach(System.out::println);
+        Map<Double, Long> entry =  result.stream().collect(Collectors.groupingBy(Integer::doubleValue , Collectors.counting()));
+        entry.entrySet().forEach(System.out::println);
+
+
+
     }
+
+
 }
